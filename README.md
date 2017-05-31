e8fd799dcf275a363bd42c0993626b38c9a32f68
WhiteheadV <zeitgeistpk@protonmail.com>
2017-05-31 17:21:56 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,30 +1,24 @@
 <h2>System monitor extension for Argos with real CPU graph</h2>
 
-This extension created specially for [Argos](https://github.com/p-e-w/argos) and linux-powered computers w/ GNOME shell.
+This is a 4rk of an extension created specially for [Argos](https://github.com/p-e-w/argos) and linux-powered computers w/ GNOME shell.
 It is based on **free**, **top**, **df** and **vmstat** output and uses power of SVG to draw charts.
+
+This specific fork has visual and other tweaks most suitable 4 arch with dark gnome theme and will probably need to be retweaked for other combinations of distros and themes.
 
 Please note that the CPU consumption is **very** approximate. Also, the real time between script execution isn't 1 sec because of latency of top and vmstat output.
 
-<h3>TODO</h3>
-
-* beautify charts with power of SVG
-* rewrite script in another lang
-* adopt code from [native System Monitor extension for GNOME Shell](https://github.com/paradoxxxzero/gnome-shell-system-monitor-applet)
-
-Do before you going to use script: `sudo apt install top sysstat`
+Install dependencies: `sudo pacman -S top sysstat` or `sudo apt install top sysstat`
+(depending on your os and package manager)
 
 <h3>Known bugs</h3>
 
-Different configurations of **top** utility doesn't allow to show processes and it's CPU consumtion (see opened issue here). 
+Different configurations of **top** utility doesn't allow to show processes and it's CPU consumtion (see opened issue here).
 To fix this, open script in text editor and replace `$9 / 2` to `$7 / 2`. Also you may need to change `head -n 10` to `head -n 13`.
-In further releases I will fix this by more elegant way.
-
+In case your **top** is not configured to sort processes by CPU usage, refer to the top(no phun intented) answer to [this](https://unix.stackexchange.com/questions/158584/change-tops-sorting-back-to-cpu) related question.
 
 <h2>Screenshot</h2>
 
-<img src="https://raw.githubusercontent.com/fadeouter/sysinfo/master/screenshot.png"> <img src="https://raw.githubusercontent.com/fadeouter/sysinfo/master/screenshot_w.png">
+![](https://lut.im/Ehimc1iQn6/GAWEv4Euy3exhLiI)
 
 <h2>License</h2>
 GNU GPL v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
-
-
--- sysinfo.1s.sh
+++ sysinfo.1s.sh
@@ -1,94 +1,67 @@
 #!/bin/bash
-#
-# this is simple and usable script from a newbie
-# based on some findings on the internet
+
+# This is a fork with visual and other tweaks most suitable 4 arch with dark gnome theme
 # based on Ganesh V BitBar script (https://github.com/ganeshv/mtop)
 # author: fadeouter (https://github.com/fadeouter/)
-# before use: sudo apt install top sysstat
+# before use: sudo apt install top sysstat or sudo pacman -S sysstat
 
-hw="16px"						   # adjust this variable to your screen DPI for appropriate SVG rendering
-diskbar_font_style="font-size='9' font-family='Cantarell'" # replace with your system font name (for disk usage bars), also as size
-symbolic="-symbolic"					   # uncomment to revert icons to multicolour
-
-### LIGHT THEME
-
-chart_color="rgba(0,0,0,0.8)"				# CPU chart main color
-pie_fg_color="rgba(0,0,0,0.5)"				# pie foreground color
-pie_bg_color="rgba(125,125,125,0.2)"			# pie background color
-hw="16px"						# adjust this variable to your screen DPI
-disk_partition_font_color="#555555"			# font color of partition mountpoint
-diskbar_font="#333333"					# font color of disk used space
-diskbar_font_highlighted="green"			# font color of disk free space
-diskbar_bg_color=$pie_bg_color				# disk bar bg color
-
-### DARK THEME
+bg_color="#33BEFF"	# pie bg color
+fg_color="#cccccc"	# pie fill color #cccccc
+# am_pie_fg_color="#abadac" #abadac
+hw="16px"		# adjust this variable to your screen DPI
 
 chart_color="rgba(255,255,255,0.8)"			# CPU chart main color
-pie_fg_color="rgba(255,255,255,0.8)"			# pie foreground color
-pie_bg_color="rgba(0,0,0,0.3)"				# pie background color	
+pie_fg_color="rgba(255,255,255,0.8)"		# pie foreground color
+pie_bg_color="rgba(0,0,0,0.3)"				# pie background color
+diskbar_font_style="font-size='8' font-family='TrueType'"	# replace TrueType with your system font name (for disk usage bars)
 disk_partition_font_color="#ccc"			# font color of partition mountpoint
-diskbar_font="white"					# font color of disk used space
+diskbar_font="#dbdbdb"					# font color of disk used space
 diskbar_font_highlighted="#7eff35"			# font color of disk free space
 diskbar_bg_color=$pie_fg_color				# disk bar bg color
 
-
 ################################################################
-#
 #  CPU
-#
 ################################################################
 
 CPU=$(echo $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]|awk '{ printf("%-4s", $1"%"); }')
-top=$(top -o "%CPU" -bn 1 | tr -d '`[]|-' | head -n 10 | tail -n 3 |  awk '{ printf("%-4s %-s\n", $9 / 2, $NF); }' | awk 1 ORS="\\\n")
+top=$(top -bn 1 | grep -v "top\|Tasks\|Cpu\|KiB" | awk '{ printf("%-4s %-s\n", $7 / 2, $11); }' | head -n 7 | tail -n+5 | awk 1 ORS="\\\\n")
 top_cpu=$(echo $top | sed 's/\\n/ /g' | awk '{ print $1 + $3 + $5}')
-
-#top -bn1 | head -n 7 | tail -n 1 | tr " " "\n" | grep -ve "^$" | awk '/CPU/{print NR}'
-
-if [ "$top_cpu" \> "$CPU" ]; then
-CPU=$(echo "$top_cpu"|awk '{ printf("%-4s", $1"%"); }')
-fi
-
-cpu_bar_height=$(echo $CPU | grep -o '[0-9]*') 
-
-
-
-########### cpu bar - uncomment this if you want to see bar and delete or comment out cpu graph part
-
-#cpu_icon=$(echo "<svg xmlns='http://www.w3.org/2000/svg' width='$hw' height='$hw' viewBox='0 0 120px 120px'><g transform='translate(0,120) scale(1, -1)'><rect rx='7px' y='10px' x='20px' height='100px' width='60px' fill='$pie_bg_color' /><rect rx='7px' y='10px' x='20px' height='$cpu_bar_height' width='60px' fill='$pie_fg_color' /></g></svg>" | base64 -w 0)
-
-
+top_tasks=$(top -bn 1 | head -n 2 | tail -n+2 | awk '{ gsub(",", "") } \
+ { printf("<b>%s</b> %d <i>%s</i> %d <i>%s</i> / %d <i>%s</i> %d <i>%s</i>\
+ ⏤ %d <i>%s</i>", $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11); }')
+cpu_bar_height=$(echo $CPU | grep -o '[0-9]*')
+cpu_bar=$(echo "<svg xmlns='http://www.w3.org/2000/svg' width='120px' hcpu_bar_height='$cpu_bar_height' height='120px' viewBox='0 0 120px 120px'><g transform='translate(0,120) scale(1, -1)'><rect rx='7px' y='10px' x='25px' height='100px' width='50px' fill='$bg_color' /><rect rx='7px' y='10px' x='25px' height='$cpu_bar_height' width='50px' fill='$fg_color' /></g></svg>" | base64 -w 0)
 
 ########### cpu graph ################
 
 HISTORY_FILE="${HOME}/.cpu.history"
 touch "${HISTORY_FILE}"
-PREVIOUS=$(tail -20 "${HISTORY_FILE}")
+PREVIOUS=$(tail -15 "${HISTORY_FILE}")
 echo "$PREVIOUS" > "${HISTORY_FILE}"
 echo "$cpu_bar_height" >> "${HISTORY_FILE}"
 
-CPU_GRAPH=$(cat $HISTORY_FILE | tr "\n" "\t" | awk '{print(100-$1,"L 5,"100-$2,"10,"100-$3,"15,"100-$4,"20,"100-$5,"25,"100-$6,"30,"100-$7,"35,"100-$8,"40,"100-$9,"45,"100-$10,"50,"100-$11,"55,"100-$12,"60,"100-$13,"65,"100-$14,"70,"100-$15,"75,"100-$16,"80,"100-$17,"85,"100-$18,"90,"100-$19,"95,"100-$20)}')
+#CPU_GRAPH=$(cat $HISTORY_FILE | tr "\n" "\t" | awk '{print(100-$1,"L 10,"100-$2,"20,"100-$3,"30,"100-$4,"40,"100-$5,"50,"100-$6,"60,"100-$7,"70,"100-$8,"80,"100-$9,"90,"100-$10,"100,"100-$11)}')
+CPU_GRAPH=$(cat $HISTORY_FILE | tr "\n" "\t" | awk '{print(100-$1,"L 10,"100-$2,"20,"100-$3,"30,"100-$4,"40,"100-$5,"50,"100-$6,"60,"100-$7,"70,"100-$8,"80,"100-$9,"90,"100-$10,"100,"100-$11,"110,"100-$12,"120,"100-$13,"130,"100-$14,"140,"100-$15,"150,"100-$16)}')
 
 #cpu_icon=$(echo "<svg xmlns='http://www.w3.org/2000/svg' width='16px' height='16px' viewBox='0 0 100 100'> <g transform='translate(0,0)'> <path style='fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#000000;stroke-opacity:1;stroke-width:2px;' d='$CPU_GRAPH' /> </g></svg>" | base64 -w 0) # graph style
 
-cpu_icon=$(echo "<svg xmlns='http://www.w3.org/2000/svg' width='28px' height='28px' viewBox='0 0 100 100'> <g transform='translate(0,0)'> <path style='fill:$chart_color;fill-opacity:1;fill-rule:evenodd;' d='M 0,100 V $CPU_GRAPH l 0,100' /> </g></svg>" | base64 -w 0) # fill style
-
+cpu_icon=$(echo "<svg xmlns='http://www.w3.org/2000/svg' width='32px' height='32px' viewBox='0 0 100 100'> <g transform='translate(0,0)'> <path style='fill:$fg_color;fill-opacity:1;fill-rule:evenodd;' d='M 0,100 V $CPU_GRAPH l 0,100' /> </g></svg>" | base64 -w 0) # fill
 ########### cpu graph end ############
 
-
-
-echo "| image=$cpu_icon imageHeight=14 imageWidth=28" #remove imageWidth property if you use bar instead of graph
+echo "$CPU | size=10 font=monospace color=#33BEFF image=$cpu_icon imageHeight=16 imageWidth=28" #remove imageWidth property if you use bar instead of graph
 
 echo "---"
-echo "<b>$CPU</b> CPU | image=$cpu_icon imageHeight=16 font=monospace size=10"
 
-echo "$top| font=monospace size=10 iconName=utilities-system-monitor$symbolic  bash=gnome-system-monitor terminal=false"
+echo "$top<span font='5'>----------------------------</span>\n<span color='#33BEFF' font='10.5'><b>$top_cpu%   <i>total</i></b></span>| font=monospace size=10 image=$cpu_icon imageHeight=36px"
+
+echo "---"
+
+echo "$top_tasks | iconName=utilities-system-monitor-symbolic font='DejaVu Sans' size=8 bash=top"
 
 echo "---"
 
 ################################################################
-#
 #  Mem
-#
 ################################################################
 
 raw_mem=$(free -m | grep Mem)
@@ -101,18 +74,18 @@ swap_full=$(echo $raw_swap | awk '{print $2}')
 swap_D=$(echo $raw_swap | awk '{print ($3 / $2) * 10 }' | awk '{ printf("%.0f\n", $1); }' | awk '{print $0"0"}')
 
 
-pie_start="<svg width='$hw' height='$hw' viewBox='0 0 90.146759 90.144005'><g transform='translate(-59.928 -103.428)'><circle cx='105' cy='148.5' r='45.979' fill='$pie_bg_color'/>"
+pie_start="<svg width='$hw' height='$hw' viewBox='0 0 90.146759 90.144005'><g transform='translate(-59.928 -103.428)'><circle cx='105' cy='148.5' r='45.979' fill='$bg_color'/>"
 pie_00="</g></svg>"
-pie_10="<path d='M105 103.52a44.98 44.98 0 0 1 26.438 8.592L105 148.5z' fill='$pie_fg_color' /></g></svg>"
-pie_20="<path d='M105 103.52a44.98 44.98 0 0 1 42.777 31.08L105 148.5z' fill='$pie_fg_color' /></g></svg>"
-pie_30="<path d='M105 103.52a44.98 44.98 0 0 1 36.388 18.542 44.98 44.98 0 0 1 6.39 40.337L105 148.5z' fill='$pie_fg_color' /></g></svg>"
-pie_40="<path d='M105 103.52a44.98 44.98 0 0 1 44.98 44.98A44.98 44.98 0 0 1 105 193.48V148.5z' fill='$pie_fg_color' /></g></svg>"
-pie_50="<path d='M105 103.52a44.98 44.98 0 0 1 44.98 44.98A44.98 44.98 0 0 1 105 193.48V148.5z' fill='$pie_fg_color' /></g></svg>"
-pie_60="<path d='M105 103.52a44.98 44.98 0 0 1 42.777 31.08 44.98 44.98 0 0 1-16.34 50.288 44.98 44.98 0 0 1-52.875 0L105 148.5z' fill='$pie_fg_color' /></g></svg>"
-pie_70="<path d='M105 103.52a44.98 44.98 0 0 1 44.732 40.278 44.98 44.98 0 0 1-35.38 48.698 44.98 44.98 0 0 1-52.13-30.097L105 148.5z' fill='$pie_fg_color' /></g></svg>"
-pie_80="<path d='M105 103.52a44.98 44.98 0 0 1 42.777 31.08 44.98 44.98 0 0 1-16.34 50.288 44.98 44.98 0 0 1-52.875 0 44.98 44.98 0 0 1-16.34-50.287L105 148.5z' fill='$pie_fg_color' /></g></svg>"
-pie_90="<path d='M105 103.52a44.98 44.98 0 0 1 44.425 37.944 44.98 44.98 0 0 1-30.526 49.813 44.98 44.98 0 0 1-53.976-22.357 44.98 44.98 0 0 1 13.638-56.808L105 148.5z' fill='$pie_fg_color' /></g></svg>"
-pie_100="<circle cx='105' cy='148.5' r='44.979' fill='$pie_fg_color' /></g></svg>"
+pie_10="<path d='M105 103.52a44.98 44.98 0 0 1 26.438 8.592L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_20="<path d='M105 103.52a44.98 44.98 0 0 1 42.777 31.08L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_30="<path d='M105 103.52a44.98 44.98 0 0 1 36.388 18.542 44.98 44.98 0 0 1 6.39 40.337L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_40="<path d='M105 103.52a44.98 44.98 0 0 1 44.98 44.98A44.98 44.98 0 0 1 105 193.48V148.5z' fill='$fg_color' /></g></svg>"
+pie_50="<path d='M105 103.52a44.98 44.98 0 0 1 44.98 44.98A44.98 44.98 0 0 1 105 193.48V148.5z' fill='$fg_color' /></g></svg>"
+pie_60="<path d='M105 103.52a44.98 44.98 0 0 1 42.777 31.08 44.98 44.98 0 0 1-16.34 50.288 44.98 44.98 0 0 1-52.875 0L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_70="<path d='M105 103.52a44.98 44.98 0 0 1 44.732 40.278 44.98 44.98 0 0 1-35.38 48.698 44.98 44.98 0 0 1-52.13-30.097L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_80="<path d='M105 103.52a44.98 44.98 0 0 1 42.777 31.08 44.98 44.98 0 0 1-16.34 50.288 44.98 44.98 0 0 1-52.875 0 44.98 44.98 0 0 1-16.34-50.287L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_90="<path d='M105 103.52a44.98 44.98 0 0 1 44.425 37.944 44.98 44.98 0 0 1-30.526 49.813 44.98 44.98 0 0 1-53.976-22.357 44.98 44.98 0 0 1 13.638-56.808L105 148.5z' fill='$fg_color' /></g></svg>"
+pie_100="<circle cx='105' cy='148.5' r='44.979' fill='$fg_color' /></g></svg>"
 
 pie_sw="pie_$swap_D"
 pie_sw=$(echo "${!pie_sw}")
@@ -122,15 +95,13 @@ pie_mem="pie_$mem_D"
 pie_mem=$(echo "${!pie_mem}")
 pie_mem=$(echo "$pie_start$pie_mem" | base64 -w 0)
 
-echo "Mem: ${mem_used%%.*} / ${mem_full%%.*} MiB | image=$pie_mem"
-echo "Swap: ${swap_used%%.*} / ${swap_full%%.*} MiB | image=$pie_sw"
+echo "<b>Mem:  </b>${mem_used%%.*} / ${mem_full%%.*} MiB | image=$pie_mem"
+echo "<b>Swap:  </b>${swap_used%%.*} / ${swap_full%%.*} MiB | image=$pie_sw"
 
 echo "---"
 
 ################################################################
-#
 #  Disk cap
-#
 ################################################################
 
 OLDIFS=$IFS
@@ -144,41 +115,34 @@ get_disk_stats() {
     local IFS=$'\n'
     local i dfdata
 
-    dfdata=($(df -H | grep "/dev/mapper*\|/dev/sd*" | grep -v "/boot\|/shm" ))
+    dfdata=($(df -H))
 
     IFS=$OLDIFS
     for ((i = 0; i < ${#dfdata[@]}; i++)); do
         line=(${dfdata[$i]})
+        if [[ "${line[0]}" == /dev/mapper* ]] || [[ "${line[0]}" == /dev/sd* ]]; then
             name+=("${line[5]}")
             cap+=("${line[1]}")
             used+=("${line[2]}")
             free+=("${line[3]}")
             capacity+=("${line[4]/\%}")
+        fi
     done
 }
 
 get_disk_stats
 
+font_style="font-size='8' font-family='TrueType'"
 height="10"
-disk_icon="drive-harddisk-system$symbolic"
 
 for ((i = 0; i < ${#capacity[@]}; i++)); do
-
-if [[ ${name[$i]} = \/media* ]]; then
-disk_icon="drive-removable-media$symbolic"
-fi
-    echo "${cap[$i]}   <span color='$disk_partition_font_color' font='10'>${name[$i]}</span> | iconName=$disk_icon length=20 bash='nautilus ${name[$i]}' terminal=false"
-    #echo "${used[$i]} / <span color='green'>${free[$i]}</span> (${capacity[$i]} %)| refresh=false  iconName=image-filter$symbolic"
-    diskbar_green=$(echo ${capacity[$i]} | awk '{print 255 - $0 * 2.55 }' | awk '{ printf("%.0f\n", $1); }')
-    diskbar_red=$(echo ${capacity[$i]} | awk '{print $0 * 2.55 }' | awk '{ printf("%.0f\n", $1); }')
-    diskbar_color="rgba($diskbar_red,$diskbar_green,0,0.7)"
-    diskbar=$(echo "<svg xmlns='http://www.w3.org/2000/svg' width='160px' height='22px' viewBox='0 0 100 11'> <rect width='100' height='2' x='0' y='$height' fill='$diskbar_bg_color' rx='1px'/> <rect width='${capacity[$i]}' height='2' x='0' y='$height' fill='$diskbar_color' rx='1px'/> <text x='0' y='7' $diskbar_font_style><tspan fill='$diskbar_font'>${used[$i]} / <tspan fill='$diskbar_font_highlighted'>${free[$i]}</tspan> (${capacity[$i]} %)</tspan></text> </svg>" | base64 -w 0)
-    echo "|image=$diskbar iconName=baobab$symbolic"
-    echo "---"
+  echo "${cap[$i]}   <span color='#70deff' font='10'>${name[$i]}</span> | iconName=drive-harddisk-symbolic length=20"
+  diskbar_green=$(echo ${capacity[$i]} | awk '{print 255 - $0 * 2.55 }' | awk '{ printf("%.0f\n", $1); }')
+  diskbar_red=$(echo ${capacity[$i]} | awk '{print $0 * 2.55 }' | awk '{ printf("%.0f\n", $1); }')
+  diskbar_color="rgba($diskbar_red,$diskbar_green,0,0.7)"
+  diskbar=$(echo "<svg xmlns='http://www.w3.org/2000/svg' width='160px' height='22px' viewBox='0 0 100 11'> <rect width='100' height='2' x='0' y='$height' fill='$diskbar_bg_color' rx='1px'/> <rect width='${capacity[$i]}' height='2' x='0' y='$height' fill='$diskbar_color' rx='1px'/> <text x='0' y='7' $diskbar_font_style><tspan fill='$diskbar_font'>${used[$i]} / <tspan fill='$diskbar_font_highlighted'>${free[$i]}</tspan> (${capacity[$i]} %)</tspan></text> </svg>" | base64 -w 0)
+  echo "|image=$diskbar refresh=false iconName=drive-removable-media-symbolic"
+  echo "---"
 done
-
-
-echo "Check free space | iconName=baobab$symbolic bash=baobab terminal=false"
-echo "Open System Monitor | iconName=utilities-system-monitor$symbolic bash=gnome-system-monitor terminal=false"
-print
-
+echo "Check free space | iconName=baobab bash=baobab terminal=false"
+echo "Open System Monitor | iconName=utilities-system-monitor bash=gnome-system-monitor terminal=false"
